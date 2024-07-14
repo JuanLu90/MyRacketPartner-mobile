198b1c0c4ba2db35910ef2d59f8aebaacf605941
Juan Luis <juanlu.calderon@gmail.com>
2024-07-14 12:23:54 +0200

    layout created

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Text } from "react-native";
+import Layout from "./src/components/Layout";
 
-export default function App() {
+function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Layout>
+      <Text>LAYOUTTTTT</Text>
+    </Layout>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+export default App;
---
+++ src/components/Layout.jsx
@@ -0,0 +1,7 @@
+import { View } from "react-native";
+
+const Layout = (props) => {
+  return <View>{props.children}</View>;
+};
+
+export default Layout;
