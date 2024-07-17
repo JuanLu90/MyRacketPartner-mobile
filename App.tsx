59cef9a7af5c3aec1cf061cb2cc58f0078487e0e
Juan Luis <juanlu.calderon@gmail.com>
2024-07-17 17:29:13 +0200

    readux toolkit added and matches slice and reducer created

--- App.tsx
+++ App.tsx
@@ -2,15 +2,20 @@ import { Text, ScrollView, StyleSheet } from "react-native";
 import Layout from "./src/components/Layout";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
+import { Provider } from "react-redux";
+import { store } from "./src/redux/store";
+
 function App() {
   return (
-    <SafeAreaProvider>
-      <ScrollView contentContainerStyle={styles.scrollViewContent}>
-        <Layout>
-          <Text style={{ color: "white" }}>LAYOUTTT</Text>
-        </Layout>
-      </ScrollView>
-    </SafeAreaProvider>
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <ScrollView contentContainerStyle={styles.scrollViewContent}>
+          <Layout>
+            <Text style={{ color: "white" }}>LAYOUTTT</Text>
+          </Layout>
+        </ScrollView>
+      </SafeAreaProvider>
+    </Provider>
   );
 }
 
